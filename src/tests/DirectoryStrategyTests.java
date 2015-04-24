421e578c61dd62eae2aaac14d2e9f7f1d864183b
Dylan Sturgeon <dylansturg@gmail.com>
2015-04-24 15:25:01 -0400

    added directory strategy test classes

---
+++ src/tests/DirectoryStrategyTests.java
@@ -0,0 +1,107 @@
+/*
+ * DirectoryStrategyTests.java
+ * Apr 24, 2015
+ *
+ * Simple Web Server (SWS) for EE407/507 and CS455/555
+ * 
+ * Copyright (C) 2011 Chandan Raj Rupakheti, Clarkson University
+ * 
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License 
+ * as published by the Free Software Foundation, either 
+ * version 3 of the License, or any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/lgpl.html>.
+ * 
+ * Contact Us:
+ * Chandan Raj Rupakheti (rupakhcr@clarkson.edu)
+ * Department of Electrical and Computer Engineering
+ * Clarkson University
+ * Potsdam
+ * NY 13699-5722
+ * http://clarkson.edu/~rupakhcr
+ */
+
+package tests;
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+
+import org.junit.Test;
+
+import configuration.ResourceStrategyRoute;
+import request.GETHTTPRequest;
+import request.HTTPRequest;
+import request.HTTPRequestFactory;
+import strategy.DirectoryStrategy;
+import strategy.IRequestTask;
+import strategy.directoryoperations.GetRequestHandler;
+
+/**
+ * 
+ * @author Chandan R. Rupakheti (rupakhcr@clarkson.edu)
+ */
+public class DirectoryStrategyTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDirectoryStrategyCreatesGetForGetRequest() {
+		DirectoryStrategy testStrategy = new DirectoryStrategy();
+		HTTPRequest testRequest = new HTTPRequestFactory()
+				.createRequest(new FakeSocket(
+						"GET /path/to/stuff HTTP/1.1\r\nHost: SomeBody.com\r\nAuthor: NotAPerson\r\n"));
+		ResourceStrategyRoute testRoute = new ResourceStrategyRoute(
+				"strategy.DirectoryStrategy", "(.*?)",
+				new HashMap<String, String>());
+
+		IRequestTask task = testStrategy.prepareEvaluation(testRequest,
+				testRoute);
+
+		assertNotNull(task);
+		assertEquals(task.getClass(), GetRequestHandler.class);
+
+	}
+
+	private class FakeSocket extends Socket {
+		String writeOutToCaller = "";
+		ByteArrayOutputStream output;
+		ByteArrayInputStream input;
+
+		public FakeSocket(String writeOut) {
+			writeOutToCaller = writeOut;
+			output = new ByteArrayOutputStream();
+			input = new ByteArrayInputStream(writeOut.getBytes(Charset
+					.defaultCharset()));
+		}
+
+		public String getWrittenString() {
+			return output.toString();
+		}
+
+		@Override
+		public InputStream getInputStream() throws IOException {
+			return input;
+		}
+
+		@Override
+		public OutputStream getOutputStream() throws IOException {
+			return output;
+		}
+	}
+}
